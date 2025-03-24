4cbc4193b8236425c67fa204ec6a54d0f6565843
mj <mjmartinsu@yahoo.es>
2025-03-24 13:59:49 +0100

    Learn testing with Vitest

--- demo1/fizzbuzz.test.ts
+++ demo1/fizzbuzz.test.ts
@@ -1,5 +1,5 @@
 import { add } from './sample';
-import { fizzbuzz } from './fizzbuzz';
+import { fizzbuzzItem, fizzbuzz } from './fizzbuzz';
 
 // Las variables start y end se dan.
 // Devuelve un array con el contenido de los números individuales.
@@ -7,30 +7,44 @@ import { fizzbuzz } from './fizzbuzz';
 // y todos los números divisibles por 5  "Fizz"
 // Si un número es divisible por 5 y 3, reemplázalo por "FizzBuzz"
 
-test('si la entrada es 3, la salida es ["fizz"]', () => {
-  expect(fizzbuzz(3)).toEqual(['fizz']);
+// Ejemplo fizzbuzz(2,6) -> [2, 'fizz', 4, 'buzz']
+
+describe('fizzbuzz one item', () => {
+  test('si la entrada es 3, la salida es "fizz"', () => {
+    expect(fizzbuzzItem(3)).toEqual('fizz');
+  });
+
+  test('si la entrada es 5, la salida es "buzz"', () => {
+    expect(fizzbuzzItem(5)).toEqual('buzz');
+  });
+
+  test('si la entrada es 2, la salida es [2]', () => {
+    expect(fizzbuzzItem(2)).toEqual([2]);
+  });
+
+  test('si la entrada es 15, la salida es "fizzbuzz"', () => {
+    expect(fizzbuzzItem(15)).toEqual('fizzbuzz');
+  });
+
+  test('si la entrada es 6, la salida es "fizz"', () => {
+    expect(fizzbuzzItem(6)).toEqual('fizz');
+  });
+
+  test('si la entrada es 10, la salida es "buzz"', () => {
+    expect(fizzbuzzItem(10)).toEqual('buzz');
+  });
+
+  test('si la entrada es 45, la salida es "fizzbuzz"', () => {
+    expect(fizzbuzzItem(45)).toEqual('fizzbuzz');
+  });
 });
 
-test('si la entrada es 5, la salida es ["buzz"]', () => {
-  expect(fizzbuzz(5)).toEqual(['buzz']);
-});
+describe('fizzbuzz', () => {
+  test('si la entrada es 1 y 2, la salida es []', () => {
+    expect(fizzbuzz(1, 2)).toStrictEqual([1, 2]);
+  });
 
-test('si la entrada es 2, la salida es [2]', () => {
-  expect(fizzbuzz(2)).toEqual([2]);
-});
-
-test('si la entrada es 15, la salida es ["fizzbuzz"]', () => {
-  expect(fizzbuzz(15)).toEqual(['fizzbuzz']);
-});
-
-test('si la entrada es 6, la salida es ["fizz"]', () => {
-  expect(fizzbuzz(6)).toEqual(['fizz']);
-});
-
-test('si la entrada es 10, la salida es ["buzz"]', () => {
-  expect(fizzbuzz(10)).toEqual(['buzz']);
-});
-
-test('si la entrada es 45, la salida es ["fizzbuzz"]', () => {
-  expect(fizzbuzz(45)).toEqual(['fizzbuzz']);
+  test('si la entrada es 1 y 3, la salida es []', () => {
+    expect(fizzbuzz(1, 3)).toStrictEqual([1, 2, 'fizz']);
+  });
 });
--- demo1/fizzbuzz.ts
+++ demo1/fizzbuzz.ts
@@ -1,9 +1,16 @@
-export const fizzbuzz = (n: number): number | string => {
-  if (n % 3 === 0 && n % 5 === 0) return ['fizzbuzz'];
+export const fizzbuzzItem = (n: number): number | string => {
+  if (n % 3 === 0 && n % 5 === 0) return 'fizzbuzz';
   //if (n === 3) return ['fizz'];
-  if (n % 3 === 0) return ['fizz'];
-  if (n === 15) return ['fizzbuzz'];
+  if (n % 3 === 0) return 'fizz';
   //if (n === 5) return ['buzz'];
-  if (n % 5 === 0) return ['buzz'];
-  return [n];
+  if (n % 5 === 0) return 'buzz';
+  return n;
+};
+
+export const fizzbuzz = (start: number, end: number): (number | string)[] => {
+  let result = [];
+  for (let i = start; i <= end; i++) {
+    result.push(...fizzbuzzItem(i));
+  }
+  return result;
 };
